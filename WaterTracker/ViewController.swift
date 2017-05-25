281af1b63e0af34878a81b1867b9546ac165bce9
Sophie Qin <sophieqin@Sophies-MacBook-Air.local>
2017-05-25 15:10:13 -0500

    Initial Commit

---
+++ WaterTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WaterTracker
+//
+//  Created by Sophie Qin on 2017-05-25.
+//  Copyright © 2017 Sophie Qin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
